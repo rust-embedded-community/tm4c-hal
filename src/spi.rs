d2e25743dea28db8c2a3ea54e74c0fb32522c99f
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-11-16 16:22:33 +0000

    Re-export Mode and MODE_x from HAL 0.2.2.

--- src/spi.rs
+++ src/spi.rs
@@ -1,18 +1,18 @@
 //! Serial Peripheral Interface (SPI) bus
 
-use crate::hal::spi::{FullDuplex, Mode, Phase, Polarity};
-use nb;
-use tm4c123x::{SSI0, SSI1, SSI2, SSI3};
+pub use crate::hal::spi::{Mode, MODE_0, MODE_1, MODE_2, MODE_3};
 
 use crate::gpio::gpioa::{PA2, PA4, PA5};
 use crate::gpio::gpiob::{PB4, PB6, PB7};
 use crate::gpio::gpiod::{PD0, PD2, PD3};
-
-use crate::sysctl;
-use crate::sysctl::Clocks;
-
 use crate::gpio::{AlternateFunction, OutputMode, AF1, AF2};
+use crate::hal::spi::{FullDuplex, Phase, Polarity};
+use crate::sysctl::Clocks;
+use crate::sysctl;
 use crate::time::Hertz;
+
+use nb;
+use tm4c123x::{SSI0, SSI1, SSI2, SSI3};
 
 /// SPI error
 #[derive(Debug)]
